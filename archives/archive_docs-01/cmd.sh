f64c318b7e777baf0d9a88ba241a27e6cc1eb340
eatondo000 <eatondo000@gmail.com>
2025-12-29 20:39:29 -0600

    update more docs

--- archives/archive_docs-01/cmd.sh
+++ archives/archive_docs-01/cmd.sh
@@ -3,5 +3,5 @@ find . -type d -name ".git" -exec rm -rf {} +
 find . -type f -size +100M -exec ls -lh {} \;
 
 git lfs migrate import --include="$(find . -type f -size +100M -printf '%P,')"
-
+git lfs migrate import --everything --above=100MB
 

98ebbb6f92dd222b2aefdb9a0760ed5b6a11a74f
eatondo000 <eatondo000@gmail.com>
2025-12-29 20:57:06 -0600

    update more docs

--- archives/archive_docs-01/cmd.sh
+++ archives/archive_docs-01/cmd.sh
@@ -5,3 +5,6 @@ find . -type f -size +100M -exec ls -lh {} \;
 git lfs migrate import --include="$(find . -type f -size +100M -printf '%P,')"
 git lfs migrate import --everything --above=100MB
 
+
+git add --renormalize .
+

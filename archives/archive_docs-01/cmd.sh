af2feca09256db00207ec8f852bbe0274cc54e96
eatondo000 <eatondo000@gmail.com>
2025-12-29 20:35:54 -0600

    update docs

--- archives/archive_docs-01/cmd.sh
+++ archives/archive_docs-01/cmd.sh
@@ -1,2 +1,7 @@
 # commands
 find . -type d -name ".git" -exec rm -rf {} +
+find . -type f -size +100M -exec ls -lh {} \;
+
+git lfs migrate import --include="$(find . -type f -size +100M -printf '%P,')"
+
+
